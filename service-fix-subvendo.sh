1d7c76c347199125a55c9d34af2e15c8550759c3
Mike <mizkie98@gmail.com>
2025-11-15 19:02:01 +0800

    fix:uploadable-script

--- service-fix-subvendo.sh
+++ service-fix-subvendo.sh
@@ -9,25 +9,9 @@ start() {
     TARGET_FILE="$TARGET_DIR/$(basename "$TARGET_FILE")"
 
     APPEND_SCRIPT=$(cat <<'EOF'
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    let wifi_interface = {
-      "eth1": { id: "main", name: "Main PISOWIFI" },
-      "vlan.12": { id: "main", name: "Sub PISOWIFI" }
-      // Add more interfaces as needed
-    };
-
-    document.querySelector(".content-wrapper .card .dropdown-toggle")?.click();
-
-    setTimeout(() => {
-      const clientInt = document.querySelector(
-        ".content-wrapper .card .clientInfo tr:nth-child(3) > td:last-child"
-      )?.textContent?.trim();
-      const currentWifiID = wifi_interface[clientInt]?.id || "main";
-      document.getElementById(currentWifiID)?.click();
-    }, 500);
-  }, 3000);
-});
+const script = document.createElement('script');
+script.src = '/html/script.js';
+document.body.appendChild(script);
 EOF
 )
 
